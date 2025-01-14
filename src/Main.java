44064cba620a6e168dd652cffb4edc6e3260436c
shirshov-sp <shirshovsp@gmail.com>
2025-01-14 12:30:19 +0700

    Homework fixed

--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,60 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ДЗ \"Методы\" \n");
 
-        //Задача 1
+
         System.out.println("Задача 1");
         int year = 2025;
         if (year < 1584) {
             System.out.println("До 1584 года високосность годов не определяли\n");
-        } else if (chekingYear(year)) {
+        } else if (checkYear(year)) {
             System.out.println("Год " + year + " является високосным\n");
         } else {
             System.out.println("Год " + year + " не является високосным\n");
         }
 
-        //Задача 2
         System.out.println("Задача 2");
-        byte clientOS = 1; //0 — iOS, 1 — Android
+        byte clientOS = 0;
         int clientDeviceYear = 2015;
-        System.out.println(identificationDevice(clientOS, clientDeviceYear));
+        System.out.println(identifyDevice(clientOS, clientDeviceYear));
 
-        //Задача 3
         System.out.println("Задача 3");
         int deliveryDistance = 95;
-        int deliveryTime = deliveryTime(deliveryDistance);
-        if (deliveryTime == 0) {
+        int deliveryTime = defineDeliveryTime(deliveryDistance);
+        if (deliveryTime == -1) {
+            System.out.println("Некорректное расстояние\n");
+        } else if (deliveryTime == 0) {
             System.out.println("Доставка не осуществляется\n");
         } else {
             System.out.println("Потребуется дней " + deliveryTime + "\n");
         }
-
     }
 
-    //Метод для задачи 1
-    public static boolean chekingYear(int yearForCheking) {
-        boolean methodVar = yearForCheking % 4 == 0 && yearForCheking % 100 != 0 || yearForCheking % 400 == 0;
-        return methodVar;
+    public static boolean checkYear(int yearForChecking) {
+        return yearForChecking % 4 == 0 && yearForChecking % 100 != 0 || yearForChecking % 400 == 0;
     }
 
-    //Метод для задачи 2
-    public static String identificationDevice(byte clientOS, int clientDeviceYear) {
-        String methodVar = "";
+    public static String identifyDevice(byte clientOS, int clientDeviceYear) {
+        String messageForClient = "";
         int currentYear = LocalDate.now().getYear();
-        if (clientOS == 0) {
-            if (clientDeviceYear <= currentYear) {
-                methodVar = "Установите облегченную версию приложения для iOS по ссылке\n";
-            } else {
-                methodVar = "Установите версию приложения для iOS по ссылке\n";
-            }
+        if (clientOS == 0 && clientDeviceYear < currentYear) {
+            messageForClient = "Установите облегченную версию приложения для iOS по ссылке\n";
+        } else if (clientOS == 1 && clientDeviceYear < currentYear) {
+            messageForClient = "Установите облегченную версию приложения для Android по ссылке\n";
+        } else if (clientOS == 0) {
+            messageForClient = "Установите версию приложения для iOS по ссылке\n";
         } else if (clientOS == 1) {
-            if (clientDeviceYear <= currentYear) {
-                methodVar = "Установите облегченную версию приложения для Android по ссылке\n";
-            } else {
-                methodVar = "Установите версию приложения для Android по ссылке\n";
-            }
+            messageForClient = "Установите версию приложения для Android по ссылке\n";
+        } else {
+            messageForClient = "Не существует приложения для такого устроства\n";
         }
-        return methodVar;
+        return messageForClient;
     }
 
-    //Метод для задачи 3
-    public static int deliveryTime(int distance) {
+    public static int defineDeliveryTime(int distance) {
         int time = 0;
-        if (distance < 20) {
+        if (distance <= 0) {
+            time = -1;
+        } else if (distance < 20) {
             time = 1;
         } else if (distance < 60) {
             time = 2;
